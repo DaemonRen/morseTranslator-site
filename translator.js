a55e196ca233607a7e1fa1b5652ed73453ec0128
Ben Augood <ben_augood@outlook.com>
2021-11-22 14:58:26 +0000

    Commit main 1
    - Add basic file structure, translator.js and testing file
    - Setup Jest, Babel etc

---
+++ translator.js
@@ -0,0 +1,8 @@
+import {alphabet} from "./alphabet.js"
+
+// 1. Make input all lowercase to match 'alphabet' (.toLowerCase)
+// 2. Split input and map it onto a new array, so it can be translated index by index
+// 3. 
+
+
+
---
+++ translator.test.js
@@ -0,0 +1,26 @@
+import {expect, it} from '@jest/globals';
+import {} from './translator.js';
+
+// If 'a' is input, Expect '.-'
+if("'a' is input, return '.-'", () => {
+    const result = translator("a");
+    expect(result).toBe(".-");
+});
+
+// If 'z' is input, Expect '--..'
+if("'z' is input, return '.-'", () => {
+    const result = translator("z");
+    expect(result).toBe("--..");
+});
+
+// If 'hello' is input, Expect '.... . .-.. .-.. ---'
+if("'hello' is input, return '.-'", () => {
+    const result = translator("hello");
+    expect(result).toBe(".... . .-.. .-.. ---");
+});
+
+// If '#^&' is input, Expect 'unavailable'
+if("'#^&' is input, return 'unavailable'", () => {
+    const result = translator("#^&");
+    expect(result).toBe("unavailable");
+});
