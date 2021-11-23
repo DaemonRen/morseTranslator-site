8a2d6ad6274c42896eacf405fc4bfc7feaee3ae7
Ben Augood <ben_augood@outlook.com>
2021-11-23 13:05:09 +0000

    Commit main 6
    - Add main script functionality
    - Add more HTML classes and input name
    - Must fix spacing on morse code output

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-import {translateFromEng} from "./translator";
+import {translateFromEng} from "./translator.js";
 
+const englishInput = document.querySelector(".english-Input");
+const englishCode = document.getElementById("englishCode")
 const englishSubmit = document.getElementById("englishToMorse__submit-btn");
-const englishInput = document.querySelector(".english-Input")
 const morseOutput = document.getElementById("morse-Translation");
 
 
 
 
 const englishToMorse = () => {
+    const input = englishInput.englishCode.value;
+    englishInput.englishCode.value = "";
+    const morseConfig = translateFromEng(input).split("   ").join("&");
+    morseOutput.innerHTML = morseConfig
+};
 
-}
+englishSubmit.addEventListener("click", englishToMorse)
